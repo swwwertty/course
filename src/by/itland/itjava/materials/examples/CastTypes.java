b52f2ab36ddea8bdf07161a1ea300ee54abc37bb
naymc <naumenkosergeyns@gmail.com>
2021-10-14 16:28:44 +0300

    addMaterials

---
+++ src/by/itland/itjava/materials/examples/CastTypes.java
@@ -0,0 +1,15 @@
+package by.itland.itjava.materials.examples;
+
+public class CastTypes {
+    public static void main(String[] args) {
+        byte b = (byte) 130;
+        byte a = (byte) 256;
+        int i = (int) 5000000000L;
+        System.out.println("b = " + b);
+        System.out.println("a = " + a);
+        System.out.println("i = " + i);
+
+        double d = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+        System.out.println(d);
+    }
+}
---
+++ src/by/itland/itjava/materials/examples/IncrementExample.java
@@ -0,0 +1,13 @@
+package by.itland.itjava.materials.examples;
+
+public class IncrementExample {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 4;
+        int c = a++ + b;
+        System.out.println("c= " + c);
+        System.out.println("a= " + a);
+    }
+}
