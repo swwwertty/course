fc8a7820b86d7cb19b0e9f74ea85a2484eaa75e2
Sasha <sasha.drobyazko.17@gmail.com>
2021-10-14 21:05:06 +0300

    first commit

---
+++ src/by/itland/itjava/drobyazko/Example1.java
@@ -0,0 +1,13 @@
+package by.itland.itjava.drobyazko;
+
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        double res = Math.pow(3, -x) - Math.cos(x) + Math.sin(2*x*y);
+        System.out.println(res);
+    }
+}
