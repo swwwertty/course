41d06bad38465290e07c31b1a9e006b4e09cd76e
Anastasia <nastatimhina@gmail.com>
2021-10-15 22:56:34 +0300

    new

---
+++ src/by/itland/itjava/Timshina/Example1.java
@@ -0,0 +1,11 @@
+package by.itland.itjava.Timshina;
+
+import java.util.Scanner;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+    }
+}
