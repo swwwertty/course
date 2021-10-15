a7c65dde023d056141eab04074c51073d1c0a76b
Anastasia <nastatimhina@gmail.com>
2021-10-15 23:35:49 +0300

    new

--- src/by/itland/itjava/Timshina/Example3.java
+++ src/by/itland/itjava/Timshina/Example3.java
@@ -2,13 +2,15 @@ package by.itland.itjava.Timshina;
 
 import java.util.Scanner;
 
-public class Example1 {
+public class Example3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         int y = scanner.nextInt();
 
-        double res = Math.pow(3, -x)-Math.cos(x)+Math.sin(2*x*y);
+        double res = (Math.pow((x+1)/(x-1), x) + 18*x*Math.pow(y, 2))/ (Math.abs(Math.pow(x, 2)-Math.pow(x, 3))- 7*x/(Math.pow(x, 3)-15*x));
+
         System.out.println(res);
+
     }
 }
