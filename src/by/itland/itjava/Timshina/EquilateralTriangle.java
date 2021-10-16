64305da0a1dec69c45a595a8f1aca2bdce2a2a07
Anastasia <nastatimhina@gmail.com>
2021-10-16 19:14:08 +0300

    new2

---
+++ src/by/itland/itjava/Timshina/EquilateralTriangle.java
@@ -0,0 +1,17 @@
+package by.itland.itjava.Timshina;
+
+import java.util.Scanner;
+
+public class EquilateralTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float x = scanner.nextFloat();
+
+        float sqr = (float) (x*Math.sqrt(3)/2);
+        float h = (float) (Math.pow(x, 2)*Math.sqrt(3)/4);
+        float r = (float) (x*Math.sqrt(3)/6);
+        float R = (float) (x*Math.sqrt(3)/3);
+
+        System.out.printf("Треугольник со стороной %.3f имеет:\n Высоту %.3f\n площадь %.3f\n радиус вписанной окружности %.3f\nрадиус описанной окружности %.3f", x, sqr, h, r, R);
+    }
+}
--- src/by/itland/itjava/Timshina/Example3.java
+++ src/by/itland/itjava/Timshina/Example3.java
@@ -1,5 +1,6 @@
 package by.itland.itjava.Timshina;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Example3 {
@@ -8,9 +9,11 @@ public class Example3 {
         int x = scanner.nextInt();
         int y = scanner.nextInt();
 
-        double res = (Math.pow((x+1)/(x-1), x) + 18*x*Math.pow(y, 2))/ (Math.abs(Math.pow(x, 2)-Math.pow(x, 3))- 7*x/(Math.pow(x, 3)-15*x));
+        double res = ( (Math.pow ((x+1), x) /(Math.pow((x-1), x)) + (18*x*Math.pow(y, 2))))
+                / ( (Math.abs (Math.pow(x, 2)-Math.pow(x, 3))) - ((7*x)/(Math.pow(x, 3)-15*x)));
 
-        System.out.println(res);
+        System.out.printf(Locale.ENGLISH, "%.3f", res);
+        //System.out.println(res);
 
     }
 }
